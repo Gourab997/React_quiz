5ddb7b53c1638f7a302b3ed3b3a11667ead0655d
Gourab997 <Gourabg998@gmail.com>
2022-06-19 17:10:28 +0600

    Quiz create layouts

--- src/components/Admin/MakeQuizs/MakeQuizs.js
+++ src/components/Admin/MakeQuizs/MakeQuizs.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import "./MakeQuizs.css";
 
 const MakeQuizs = () => {
-  const [inputList, setInputList] = useState([{ firstName: "", lastName: "" }]);
+  const [inputList, setInputList] = useState([
+    { question: "", answer1: "", answer2: "", answer3: "", answer4: "" },
+  ]);
   const {
     register,
     handleSubmit,
@@ -25,7 +28,10 @@ const MakeQuizs = () => {
 
   // handle click event of the Add button
   const handleAddClick = () => {
-    setInputList([...inputList, { firstName: "", lastName: "" }]);
+    setInputList([
+      ...inputList,
+      { question: "", answer1: "", answer2: "", answer3: "", answer4: "" },
+    ]);
   };
   const onSubmit = (data) => {
     data.quizs = inputList;
@@ -40,37 +46,117 @@ const MakeQuizs = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div class="mb-3">
           <label for="exampleFormControlInput1" class="form-label">
-            Email address
+            Quiz Title
           </label>
           <input
-            type="email"
+            type="text"
             class="form-control"
             id="exampleFormControlInput1"
-            placeholder="name@example.com"
+            placeholder="Quiz Title"
             {...register("QuizTitle")}
           />
         </div>
 
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
         {inputList.map((x, i) => {
           return (
-            <div className="box">
-              <input
-                name="firstName"
-                placeholder="Enter First Name"
-                value={x.firstName}
-                onChange={(e) => handleInputChange(e, i)}
-              />
-              <input
-                className="ml10"
-                name="lastName"
-                placeholder="Enter Last Name"
-                value={x.lastName}
-                onChange={(e) => handleInputChange(e, i)}
-              />
+            <div className="card my-4">
+              <div className="container">
+                <div className="d-flex justify-content-evenly">
+                  <label>Question</label>
+                  <input
+                    name="question"
+                    class="card-title"
+                    placeholder="Question"
+                    value={x.question}
+                    onChange={(e) => handleInputChange(e, i)}
+                  />
+                </div>
+                <div className="container">
+                  <div className="d-flex justify-content-between my-3">
+                    <input
+                      className="ml10"
+                      name="answer1"
+                      placeholder="Option 1"
+                      value={x.answer1}
+                      onChange={(e) => handleInputChange(e, i)}
+                    />
+                    <div class="form-check ">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="flexCheckDefault"
+                      />
+                      <label class="form-check-label" for="flexCheckDefault">
+                        Answer
+                      </label>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-between my-3">
+                    <input
+                      className="ml10"
+                      name="answer1"
+                      placeholder="Option 1"
+                      value={x.answer1}
+                      onChange={(e) => handleInputChange(e, i)}
+                    />
+                    <div class="form-check ">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="flexCheckDefault"
+                      />
+                      <label class="form-check-label" for="flexCheckDefault">
+                        Answer
+                      </label>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-between my-3">
+                    <input
+                      className="ml10"
+                      name="answer1"
+                      placeholder="Option 1"
+                      value={x.answer1}
+                      onChange={(e) => handleInputChange(e, i)}
+                    />
+                    <div class="form-check ">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="flexCheckDefault"
+                      />
+                      <label class="form-check-label" for="flexCheckDefault">
+                        Answer
+                      </label>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-between my-3">
+                    <input
+                      className="ml10"
+                      name="answer1"
+                      placeholder="Option 1"
+                      value={x.answer1}
+                      onChange={(e) => handleInputChange(e, i)}
+                    />
+                    <div class="form-check ">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="flexCheckDefault"
+                      />
+                      <label class="form-check-label" for="flexCheckDefault">
+                        Answer
+                      </label>
+                    </div>
+                  </div>
+
+               
+                </div>
+              </div>
+
               <div className="btn-box">
                 {inputList.length !== 1 && (
                   <button className="mr10" onClick={() => handleRemoveClick(i)}>
