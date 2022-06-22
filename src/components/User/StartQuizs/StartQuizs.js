d04f7e34b7aa4ff2de11fcce4fe60991482f4d2e
Gourab997 <Gourabg998@gmail.com>
2022-06-22 18:27:08 +0600

    quiz answer submit

--- src/components/User/StartQuizs/StartQuizs.js
+++ src/components/User/StartQuizs/StartQuizs.js
@@ -1,43 +1,79 @@
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
-
 const StartQuizs = () => {
   const { quizId } = useParams([]);
   const [getquiz, setGetquiz] = useState();
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm();
   useEffect(() => {
     fetch(`http://localhost:5000/quiz/${quizId}`)
       .then((res) => res.json())
       .then((data) => setGetquiz(data));
   }, [getquiz]);
-
+  const onDataSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      {getquiz &&
-        getquiz.quizs.map((qus, i) => (
-          <div>
-            <div class="card-body">
-              <h5 class="card-title">{qus.question}</h5>
-              <div>
-                <input class="form-check-input" type="radio" name={i} id={i} />
-                <label class="form-check-label" for={i}>
-                  {qus.option1}
-                </label>
-                <input class="form-check-input" type="radio" name={i} id={i} />
-                <label class="form-check-label" for={i}>
-                  {qus.option2}
-                </label>
-                <input class="form-check-input" type="radio" name={i} id={i} />
-                <label class="form-check-label" for={i}>
-                  {qus.option3}
-                </label>
-                <input class="form-check-input" type="radio" name={i} id={i} />
-                <label class="form-check-label" for={i}>
-                  {qus.option4}
-                </label>
+      <form onSubmit={handleSubmit(onDataSubmit)}>
+        {getquiz &&
+          getquiz.quizs.map((qus, i) => (
+            <div>
+              <div class="card-body">
+                <h5 class="card-title">{qus.question}</h5>
+
+                <div>
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name={i}
+                    value="A"
+                    {...register(`anwser${i}`)}
+                  />
+                  <label class="form-check-label" for={i}>
+                    {qus.option1}
+                  </label>
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name={i}
+                    value="B"
+                    {...register(`anwser${i}`)}
+                  />
+                  <label class="form-check-label" for={i}>
+                    {qus.option2}
+                  </label>
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name={i}
+                    {...register(`anwser${i}`)}
+                    value="C"
+                  />
+                  <label class="form-check-label" for={i}>
+                    {qus.option3}
+                  </label>
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name={i}
+                    {...register(`anwser${i}`)}
+                    value="D"
+                  />
+                  <label class="form-check-label" for={i}>
+                    {qus.option4}
+                  </label>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
